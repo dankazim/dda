494dc61ecca830ab497b62230b7a6abe229d6952
Dan M. Kazimoto <itskazimz@gmail.com>
2021-03-04 08:12:00 +0000

    add and view notes completed.

--- main.py
+++ main.py
@@ -1,58 +1,45 @@
-list_note = {"Mar 2 ": {"Content": "Working on Note Keeper", "Labels": "Programming, testing, default"}, "Mar 3": {"Content": "Respiratory", "Labels": "Biology, Systolic, Balloon, default"}}
-detail_note = ["Title", "Content", "Labels"]
+from datetime import datetime
+TimeToString = datetime.now().strftime("%d/%m/%Y %H:%M")
 
 
-line = "============================="
-
-def show_notes():
-  print("\nList of all notes:")
-  for i in list_note:
-    print(f"\nTitle: {i}\n{line}")
-    view_note(i)
-
-def view_note(title = ""):
-  if len(title) < 1:
-    title = str(input("Enter the title: "))
-  # print(list_note[title])
-  for k, v in list_note[title].items():
-    print(f"{k}: {v}")  
+notes = {"Mar 2 ": {"Date": "march 4", "Content": "Working on Note Keeper", "Labels": "Programming, testing, default"}}
+notes_template = ["Date", "Title", "Descriptions", "Labels"]
 
 
-def create_update_note():
-  temp = []
-  print("")
-  for i in range(0, 3):
-    # print(f"Enter {detail_note[i]}: ")
-    temp.append(str(input(f"Enter the {detail_note[i]}: ")))
-  title, content, labels = temp
-  # title = "Mac 4"; content = "Watching youtube video"; labels = "YouTube, Google"
-  list_note[title] = {detail_note[1]: content, detail_note[2]: labels}
-  print(f"{title} has been added/updated!")
+def displayNotes():
+  for key in notes:
+    print(f'{key}: {notes[key]}')
+
+def updateNote():
+  addNote()
+  notes[title] = {notes_template[0]: TimeToString, notes_template[2]: labels, notes_template[3]: labels}
   
+def deleteNote(title):
+  notes.pop(title)
 
-def del_note():
-  title = str(input("Enter the title: "))
-  list_note.pop(title)
-  print(f"{title} has been deleted!")
+def addNote():
+  title = str(input("Title: "))
+  descriptions = str(input("Description: "))
+  labels = str(input("Label: "))
+  notes[title] = {notes_template[0]: TimeToString, notes_template[2]: descriptions, notes_template[3]: labels}
+  return title, descriptions,labels
 
 
-def menu_option(selection):
-  menu = {
-    'C': create_update_note, 
-    'R': show_notes, 
-    'U': create_update_note, 
-    'D': del_note
-    }
-  menu.get(selection, "Invalid input")()
-
+  
+def processInput():
+  if option ==1:
+    addNote()
+    print("a new note was created")
+  elif option ==2:
+    displayNotes()
+  elif option ==3:
+    updateNote()
+  elif option ==4:
+    deleteNote
+    
 while True:
-  print("\nWhat would you like to do?\nC: Create a note\nR: List title of notes; and show a note\nU: Update a note\nD: Remove a note")
-  selection = str(input("Your input: "))
-  menu_option(selection)
-
-
-
-# create_update_note()
-# show_notes()
+  print("\nWhat would you like to do?\n1: Create a new note.\n2: List all notes.\n3: Update a note.\n4: Remove a note")
+  option = int(input("Your choice(1,2,3 or 4): "))
+  processInput()
 
 
